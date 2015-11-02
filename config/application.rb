9d68bd53915511df7c591626b1d06a9190d59b2a
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-11-02 20:46:47 +0300

    fix smtp

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Redshell
 
     config.time_zone = 'Moscow'
 
+    config.action_mailer.delivery_method = :smtp
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.before_configuration do
     #   I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
