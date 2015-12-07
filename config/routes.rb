d58ce8cef9f55028f8ad3f230b3d96e45dea0f7d
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-12-07 14:57:54 +0300

    add storytime

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
@@ -58,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  mount Storytime::Engine => '/'
 end
